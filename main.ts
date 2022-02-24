8f7edaa4a9896117492f31bd371583d57a7d0396
CaptainArthur <100374377+CaptainArthur@users.noreply.github.com>
2022-02-24 19:28:12 +0100

    1. build

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-
+input.onButtonPressed(Button.A, function () {
+    let Teplota = 0
+    if (Teplota > 0 < 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
+            `)
+    }
+})
