38e62f669a70ab574debd65cea6ff455ec366dd2
MatoJeTu <100374467+MatoJeTu@users.noreply.github.com>
2022-03-02 21:21:15 +0100

    Aktualizovať main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+teplota = 0
+
 def on_button_pressed_a():
-    if True:
-        radio.send_value("name", randint(0, 10))
+    radio.send_value("name", input.temperature())
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_received_value(name, value):
-    basic.show_number(randint(0, 10))
-    basic.pause(1000)
-    basic.clear_screen()
+    global teplota
+    teplota = input.temperature()
 radio.on_received_value(on_received_value)
--- main.ts
+++ main.ts
@@ -1,10 +1,6 @@
-let teplota = 0
 input.onButtonPressed(Button.A, function () {
-    teplota = input.temperature()
-    if (true) {
-        radio.sendValue("name", teplota)
-    }
+	
 })
 radio.onReceivedValue(function (name, value) {
-    basic.showNumber(teplota)
+	
 })
